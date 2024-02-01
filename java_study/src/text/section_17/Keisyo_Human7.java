9f8f90ba12c3dc5316bd4ff31eb8cf39f6c91790
ycnitcir <157781799+ycnitcir@users.noreply.github.com>
2024-02-01 18:19:07 +0900

    Delete java_study directory

--- java_study/src/text/section_17/Keisyo_Human7.java
+++
@@ -1,6 +0,0 @@
-package text.section_17;
-
-abstract public class Keisyo_Human7 {
-	 //抽象メソッド
-	   abstract void output();
-}
--- java_study/src/text/section_17/Keisyo_Tanaka7.java
+++
@@ -1,7 +0,0 @@
-package text.section_17;
-
-public class Keisyo_Tanaka7 extends Keisyo_Human7{
-	 void output() {
-	      System.out.println("Tanaka");
-	   }
-}
